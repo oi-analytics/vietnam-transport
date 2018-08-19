a72a36f9fb594de75ac6d84617d0a5f802fe071c
itrcrisks <raghavp@gmail.com>
2018-08-20 00:21:13 +0100

    new functions to utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -24,6 +24,11 @@ import fiona
 import fiona.crs
 import rasterio
 
+from scipy.spatial import Voronoi
+import shapely.geometry
+import shapely.ops
+from shapely.geometry import Polygon
+
 def load_config():
     """Read config.json
     """
@@ -400,8 +405,9 @@ def gdf_clip(shape_in,clip_geom):
     gdf = gpd.read_file(shape_in)
     gdf = gdf.to_crs({'init': 'epsg:4326'})
     return gdf.loc[gdf['geometry'].apply(lambda x: x.within(clip_geom))].reset_index(drop=True)
- 
-def get_nearest_node(x,sindex_nodes,nodes,id_column):
+
+
+def get_nearest_node(x,sindex_input_nodes,input_nodes,id_column):
     """
     Inputs are:
         x -- row of dataframe
@@ -411,7 +417,7 @@ def get_nearest_node(x,sindex_nodes,nodes,id_column):
     Outputs are:
         Nearest node to geometry of row
     """
-    return nodes.loc[list(sindex_nodes.nearest(x.bounds[:2]))][id_column].values[0]
+    return input_nodes.loc[list(sindex_input_nodes.nearest(x.bounds[:2]))][id_column].values[0]
 
 def count_points_in_polygon(x,points_sindex):
     """
@@ -435,3 +441,101 @@ def extract_value_from_gdf(x,gdf_sindex,gdf,column_name):
         extracted value from other gdf
     """
     return gdf.loc[list(gdf_sindex.intersection(x.bounds[:2]))][column_name].values[0]
+
+def assign_value_in_area_proportions(poly_1_gpd,poly_2_gpd,poly_attribute):
+    poly_1_sindex = poly_1_gpd.sindex
+    for p_2_index, polys_2 in poly_2_gpd.iterrows():
+        poly2_attr= 0
+        intersected_polys = poly_1_gpd.iloc[list(poly_1_sindex.intersection(polys_2.geometry.bounds))]
+        for p_1_index, polys_1 in intersected_polys.iterrows():
+            if (polys_2['geometry'].intersects(polys_1['geometry']) is True) and (polys_1.geometry.is_valid is True) and (polys_2.geometry.is_valid is True):
+                poly2_attr += polys_1[poly_attribute]*polys_2['geometry'].intersection(polys_1['geometry']).area/polys_1['geometry'].area
+        
+        poly_2_gpd.loc[p_2_index,poly_attribute] = poly2_attr
+
+    return poly_2_gpd
+
+def voronoi_finite_polygons_2d(vor, radius=None):
+    """
+    Copy-pasted from: https://stackoverflow.com/questions/36063533/clipping-a-voronoi-diagram-python
+
+    Reconstruct infinite voronoi regions in a 2D diagram to finite
+    regions.
+    Parameters
+    ----------
+    vor : Voronoi
+        Input diagram
+    radius : float, optional
+        Distance to 'points at infinity'.
+    Returns
+    -------
+    regions : list of tuples
+        Indices of vertices in each revised Voronoi regions.
+    vertices : list of tuples
+        Coordinates for revised Voronoi vertices. Same as coordinates
+        of input vertices, with 'points at infinity' appended to the
+        end.
+    """
+
+    if vor.points.shape[1] != 2:
+        raise ValueError("Requires 2D input")
+
+    new_regions = []
+    new_vertices = vor.vertices.tolist()
+
+    center = vor.points.mean(axis=0)
+    if radius is None:
+        radius = vor.points.ptp().max()*2
+
+    # Construct a map containing all ridges for a given point
+    all_ridges = {}
+    for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
+        all_ridges.setdefault(p1, []).append((p2, v1, v2))
+        all_ridges.setdefault(p2, []).append((p1, v1, v2))
+
+    # Reconstruct infinite regions
+    for p1, region in enumerate(vor.point_region):
+        vertices = vor.regions[region]
+
+        if all(v >= 0 for v in vertices):
+            # finite region
+            new_regions.append(vertices)
+            continue
+
+        # reconstruct a non-finite region
+        ridges = all_ridges[p1]
+        new_region = [v for v in vertices if v >= 0]
+
+        for p2, v1, v2 in ridges:
+            if v2 < 0:
+                v1, v2 = v2, v1
+            if v1 >= 0:
+                # finite ridge: already in the region
+                continue
+
+            # Compute the missing endpoint of an infinite ridge
+
+            t = vor.points[p2] - vor.points[p1] # tangent
+            t /= np.linalg.norm(t)
+            n = np.array([-t[1], t[0]])  # normal
+
+            midpoint = vor.points[[p1, p2]].mean(axis=0)
+            direction = np.sign(np.dot(midpoint - center, n)) * n
+            far_point = vor.vertices[v2] + direction * radius
+
+            new_region.append(len(new_vertices))
+            new_vertices.append(far_point.tolist())
+
+        # sort region counterclockwise
+        vs = np.asarray([new_vertices[v] for v in new_region])
+        c = vs.mean(axis=0)
+        angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
+        new_region = np.array(new_region)[np.argsort(angles)]
+
+        # finish
+        new_regions.append(new_region.tolist())
+
+    return new_regions, np.asarray(new_vertices)
+
+def extract_nodes_within_gdf(x,input_nodes,column_name):
+    return input_nodes.loc[list(input_nodes.geometry.within(x.geometry))][column_name].values[0]
