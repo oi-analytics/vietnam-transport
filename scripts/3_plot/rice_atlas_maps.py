b7ed54eb0a5280be7e366d63ea3e2cd42b5ccbf9
itrcrisks <raghavp@gmail.com>
2018-09-04 15:09:20 +0100

    new scripts for plotting crop tonnages and mapping all sector-wise flows to same scales

---
+++ scripts/3_plot/rice_atlas_maps.py
@@ -0,0 +1,81 @@
+"""Generate crop maps for Vietnam
+"""
+# pylint: disable=C0103
+import os
+import sys
+
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib as mpl
+
+mpl.style.use('ggplot')
+mpl.rcParams['font.size'] = 11.
+
+#mpl.rcParams['font.family'] = 'tahoma'
+mpl.rcParams['axes.labelsize'] = 14.
+mpl.rcParams['xtick.labelsize'] = 11.
+mpl.rcParams['ytick.labelsize'] = 11.
+mpl.rcParams['savefig.pad_inches'] = 0.05
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from scripts.utils import *
+
+def main():
+	config = load_config()
+	crop_month_fields = ['P_Jan','P_Feb','P_Mar','P_Apr','P_May','P_Jun','P_Jul','P_Aug','P_Sep','P_Oct','P_Nov','P_Dec']
+	title_cols = ['January','February','March','April','May','June','July','August','September','October','November','December']
+	for cr in range(len(crop_month_fields)):
+		rice_month_file = os.path.join(config['paths']['data'],'rice_atlas_vietnam','rice_production.shp')
+		ax = get_axes()
+		plot_basemap(ax, config['paths']['data'])
+		scale_bar(ax, location=(0.8, 0.05))
+		plot_basemap_labels(ax, config['paths']['data'])
+		proj = ccrs.PlateCarree()
+		for record in shpreader.Reader(rice_month_file).records():
+			geom = record.geometry
+			region_val = 100.0*record.attributes[crop_month_fields[cr]]/record.attributes['P_total']
+			if region_val:
+				if region_val > 0 and region_val <= 20:
+					color = '#ffffcc' # TODO
+					ax.add_geometries([geom], crs=proj, edgecolor='#ffffff', facecolor=color,label = '0 to 20')
+				elif region_val > 20 and region_val <= 40:
+					color = '#c2e699' # TODO
+					ax.add_geometries([geom], crs=proj, edgecolor='#ffffff', facecolor=color,label = '20 to 40')
+				if region_val > 40 and region_val <= 60:
+					color = '#78c679' # TODO
+					ax.add_geometries([geom], crs=proj, edgecolor='#ffffff', facecolor=color,label = '40 to 60')
+				elif region_val > 60 and region_val <= 80:
+					color = '#31a354' # TODO
+					ax.add_geometries([geom], crs=proj, edgecolor='#ffffff', facecolor=color,label = '60 to 80')
+				elif region_val > 80 and region_val <= 100:
+					color = '#31a354' # TODO
+					ax.add_geometries([geom], crs=proj, edgecolor='#ffffff', facecolor=color,label = '80 to 100')
+		
+			else:
+				ax.add_geometries([geom], crs=proj, edgecolor='#ffffff', facecolor='#ffffcc',label = '0 to 20')
+
+
+		colors = ['#ffffcc','#c2e699','#78c679','#31a354','#31a354']
+		labels = ['0 to 20','20 to 40','40 to 60','60 to 80','80 to 100']
+		# Legend
+		legend_handles = []
+		for c in range(len(colors)):
+			legend_handles.append(mpatches.Patch(color=colors[c], label=labels[c]))
+			
+		ax.legend(
+			handles=legend_handles,
+			title = 'Percentage production',
+			loc='center left'
+			)
+
+				
+		plt.title(title_cols[cr], fontsize = 14)
+		output_file = os.path.join(config['paths']['figures'], 'rice_production_{}.png'.format(title_cols[cr]))
+		save_fig(output_file)
+		plt.close()
+
+if __name__ == '__main__':
+	main()
---
+++ scripts/3_plot/road_network_flows_max_scales.py
@@ -0,0 +1,161 @@
+"""Road network flows
+"""
+import os
+import sys
+
+from collections import OrderedDict
+
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+import matplotlib.pyplot as plt
+from shapely.geometry import LineString
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from scripts.utils import *
+
+def main():
+    config = load_config()
+    flows_file = os.path.join(config['paths']['data'], 'Results', 'Flow_shapefiles', 'weighted_edges_flows_national_road.shp')
+
+    plot_sets = [
+        {
+            'file_tag': 'vehicle_count',
+            'legend_label': "AADT ('000 vehicles/day)",
+            'divisor': 1000,
+            'columns': ['vehicle_co'],
+            'title_cols': ['Vehicle Count']
+        },
+        {
+            'file_tag': 'commodities',
+            'legend_label': "AADF ('000 tons/day)",
+            'divisor': 1000,
+            'columns': ['max_rice','max_cash','max_cass','max_teas','max_maiz',
+                        'max_rubb','max_swpo','max_acof','max_rcof','max_pepp', 
+                        'max_sugar','max_wood','max_steel','max_constr','max_cement',
+                        'max_fertil','max_coal','max_petrol','max_manufa','max_fisher',
+                        'max_meat', 'max_tons'],
+            'title_cols': ['Rice','Cashew','Cassava','Teas','Maize','Rubber',
+                           'Sweet Potatoes','Coffee Arabica','Coffee Robusta',
+                           'Pepper','Sugar','Wood','Steel','Construction materials',
+                           'Cement','Fertilizer','Coal','Petroleum','Manufacturing',
+                           'Fishery','Meat','Total tonnage']
+        },
+    ]
+    
+    for plot_set in plot_sets:
+        for c in range(len(plot_set['columns'])):
+            ax = get_axes()
+            plot_basemap(ax, config['paths']['data'])
+            scale_bar(ax, location=(0.8, 0.05))
+            plot_basemap_labels(ax, config['paths']['data'])
+            proj_lat_lon = ccrs.PlateCarree()
+
+            # generate weight bins
+            if plot_set['columns'][c] == 'vehicle_co':
+                column = plot_set['columns'][c]
+            else:
+                column = 'max_tons'
+            
+            weights = [
+                record.attributes[column]
+                for record in shpreader.Reader(flows_file).records()
+            ]
+            max_weight = max(weights)
+            width_by_range = generate_weight_bins(weights)
+
+            road_geoms_by_category = {
+                '1': [],
+                '2': [],
+                '3': [],
+                '4': [],
+                '5': [],
+                '6': []
+            }
+
+            column = plot_set['columns'][c]
+            for record in shpreader.Reader(flows_file).records():
+                cat = str(record.attributes['road_class'])
+                if cat not in road_geoms_by_category:
+                    raise Exception
+                geom = record.geometry
+
+                val = record.attributes[column]
+
+                buffered_geom = None
+                for (nmin, nmax), width in width_by_range.items():
+                    if nmin <= val and val < nmax:
+                        buffered_geom = geom.buffer(width)
+
+                if buffered_geom is not None:
+                    road_geoms_by_category[cat].append(buffered_geom)
+                else:
+                    print("Feature was outside range to plot", record.attributes)
+
+            styles = OrderedDict([
+                ('1',  Style(color='#000004', zindex=9, label='Class 1')), #red
+                ('2', Style(color='#2c115f', zindex=8, label='Class 2')), #orange
+                ('3', Style(color='#721f81', zindex=7, label='Class 3')), #blue
+                ('4',  Style(color='#b73779', zindex=6, label='Class 4')), #green
+                ('5', Style(color='#f1605d', zindex=5, label='Class 5')), #black
+                ('6', Style(color='#feb078', zindex=4, label='Class 6')), #grey
+            ])
+
+            for cat, geoms in road_geoms_by_category.items():
+                cat_style =styles[cat]
+                ax.add_geometries(
+                    geoms,
+                    crs=proj_lat_lon,
+                    linewidth=0,
+                    facecolor=cat_style.color,
+                    edgecolor='none',
+                    zorder=cat_style.zindex
+                )
+
+            x_l = 102.3
+            x_r = x_l + 0.4
+            base_y = 14
+            y_step = 0.4
+            y_text_nudge = 0.1
+            x_text_nudge = 0.1
+
+            ax.text(
+                x_l,
+                base_y + y_step - y_text_nudge,
+                plot_set['legend_label'],
+                horizontalalignment='left',
+                transform=proj_lat_lon,
+                size=10)
+
+            divisor = plot_set['divisor']
+            for (i, ((nmin, nmax), width)) in enumerate(width_by_range.items()):
+                y = base_y - (i*y_step)
+                line = LineString([(x_l, y), (x_r, y)]).buffer(width)
+                ax.add_geometries(
+                    [line],
+                    crs=proj_lat_lon,
+                    linewidth=0,
+                    edgecolor='#000000',
+                    facecolor='#000000',
+                    zorder=2)
+                if nmin == max_weight:
+                    label = '>{:.2f}'.format(max_weight/divisor)
+                else:
+                    label = '{:.2f}-{:.2f}'.format(nmin/divisor, nmax/divisor)
+                ax.text(
+                    x_r + x_text_nudge,
+                    y - y_text_nudge,
+                    label,
+                    horizontalalignment='left',
+                    transform=proj_lat_lon,
+                    size=10)
+
+            plt.title(plot_set['title_cols'][c], fontsize = 14)
+            legend_from_style_spec(ax, styles)
+            output_file = os.path.join(config['paths']['figures'], 'road_flow-map-{}-{}-max-scale.png'.format(plot_set['file_tag'], column))
+            save_fig(output_file)
+            plt.close()
+
+
+
+if __name__ == '__main__':
+    main()
