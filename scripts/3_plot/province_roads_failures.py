b3eaf380b18e1b1baa78692775860bf671397ccf
itrcrisks <raghavp@gmail.com>
2018-09-05 15:21:30 +0100

    added new scripts and midified existing ones

---
+++ scripts/3_plot/province_roads_failures.py
@@ -0,0 +1,160 @@
+"""Road network flows
+"""
+import os
+import sys
+
+from collections import OrderedDict
+
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+import matplotlib.pyplot as plt
+from shapely.geometry import LineString
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from scripts.utils import *
+
+def main():
+    config = load_config()
+
+    regions = ['Lao Cai', 'Binh Dinh', 'Thanh Hoa']
+    plot_set = [
+        {
+            'column': 'min_econ_l',
+            'title': 'Min Economic loss',
+            'legend_label': "('000 USD/day)",
+            'divisor': 1000,
+            'significance': 0
+        },
+        {
+            'column': 'max_econ_l',
+            'title': 'Max Economic loss',
+            'legend_label': "('000 USD/day)",
+            'divisor': 1000,
+            'significance': 0
+        },
+        {
+            'column': 'min_tons',
+            'title': 'Min Tons',
+            'legend_label': "MT (tons/day)",
+            'divisor': 1,
+            'significance': 0
+        },
+        {
+            'column': 'max_tons',
+            'title': 'Max Tons',
+            'legend_label': "MT (tons/day)",
+            'divisor': 1,
+            'significance': 0
+        }
+    ]
+    
+    for region in regions:
+
+        region_file = os.path.join(config['paths']['data'], 'Results', 'Failure_shapefiles', 'weighted_edges_commune_center_failures_' + region.lower().replace(' ', '') + '_5_tons.shp')
+        plot_settings = get_region_plot_settings(region)
+
+        for c in range(len(plot_set)):
+            ax = get_axes(plot_settings['bbox'], figsize=plot_settings['figure_size'])
+
+            if region == 'Binh Dinh':
+                plot_basemap(ax, config['paths']['data'], country_border='none', plot_states=False)
+            else:
+                plot_basemap(ax, config['paths']['data'], country_border='none', plot_states=True)
+        
+            scale_bar(ax, location=(0.8, 0.05), length=plot_settings['scale_legend'])
+            proj_lat_lon = ccrs.PlateCarree()
+
+            # generate weight bins
+            column = plot_set[c]['column']
+            weights = [
+                record.attributes[column]
+                for record in shpreader.Reader(region_file).records()
+            ]
+            max_weight = max(weights)
+            width_by_range = generate_weight_bins_with_colour_gradient(weights, width_step=0.001)
+
+            road_geoms_by_category = {
+                region: []
+            }
+
+            styles = OrderedDict([
+                (region,  Style(color='#ba0f03', zindex=6, label=region))
+            ])
+
+            for record in shpreader.Reader(region_file).records():
+                cat = region
+                geom = record.geometry
+
+                val = record.attributes[column]
+
+                buffered_geom = None
+                for (nmin, nmax), line_style in width_by_range.items():
+                    if nmin <= val and val < nmax:
+                        buffered_geom = geom.buffer(line_style[1])
+
+                        if buffered_geom is not None:
+                            ax.add_geometries(
+                                [buffered_geom],
+                                crs=proj_lat_lon,
+                                linewidth=0,
+                                facecolor=str(line_style[2]),
+                                edgecolor='none',
+                                zorder=3 + line_style[0]
+                            )
+                        else:
+                            print("Feature was outside range to plot", record.attributes)
+
+            x_l = plot_settings['weight_legend']['x_l']
+            x_r = plot_settings['weight_legend']['x_r']
+            base_y = plot_settings['weight_legend']['base_y']
+            y_step = plot_settings['weight_legend']['y_step']
+            y_text_nudge = plot_settings['weight_legend']['y_text_nudge']
+            x_text_nudge = plot_settings['weight_legend']['x_text_nudge']
+
+            # text above weight legend
+            ax.text(
+                x_l,
+                base_y + y_step - y_text_nudge,
+                plot_set[c]['legend_label'],
+                horizontalalignment='left',
+                transform=proj_lat_lon,
+                size=10)
+
+            # weight legend            
+            divisor = plot_set[c]['divisor']
+
+            for (i, ((nmin, nmax), line_style)) in enumerate(width_by_range.items()):
+                y = base_y - (i*y_step)
+                line = LineString([(x_l, y), (x_r, y)]).buffer(line_style[1])
+                ax.add_geometries(
+                    [line],
+                    crs=proj_lat_lon,
+                    linewidth=0,
+                    edgecolor=str(line_style[2]),
+                    facecolor=str(line_style[2]),
+                    zorder=2)
+                significance_ndigits = plot_set[c]['significance']
+                if nmin == max_weight:
+                    value_template = '>{:.' + str(significance_ndigits) + 'f}'
+                    label = value_template.format(round(max_weight/divisor, significance_ndigits))
+                else:
+                    value_template = '{:.' + str(significance_ndigits) + 'f}-{:.' + str(significance_ndigits) + 'f}'
+                    label = value_template.format(round(nmin/divisor, significance_ndigits), round(nmax/divisor, significance_ndigits))
+                ax.text(
+                    x_r + x_text_nudge,
+                    y - y_text_nudge,
+                    label,
+                    horizontalalignment='left',
+                    transform=proj_lat_lon,
+                    size=10)
+
+            # plot
+            plt.title(region + ' (' + plot_set[c]['title'] + ')', fontsize = 14) 
+
+            # output
+            output_file = os.path.join(config['paths']['figures'], 'commune_center-{}-{}-failures.png'.format(region.lower().replace(' ', ''), column))
+            save_fig(output_file)
+            plt.close()
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/3_plot/rail_network_failures.py
@@ -0,0 +1,142 @@
+"""Rail network flows map
+"""
+import os
+import sys
+
+from collections import OrderedDict
+
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+import matplotlib.pyplot as plt
+import numpy as np
+from shapely.geometry import LineString
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from scripts.utils import *
+
+def main():
+	config = load_config()
+	flows_file = os.path.join(config['paths']['data'], 'Results', 'Failure_shapefiles', 'weighted_edges_failures_national_rail.shp')
+	
+	plot_sets = [
+		{
+			'file_tag': 'reroute',
+			'no_access': [-1,0],
+			'legend_label': "(million USD/day)",
+			'divisor': 1000000,
+			'columns': ['min_tr_los','max_tr_los'],
+			'title_cols': ['Rerouting costs (min)','Rerouting costs (max)']
+		},
+		{
+			'file_tag': 'commodities',
+			'no_access':[-1,1],
+			'legend_label': "AADF (tons/day)",
+			'divisor': 1,
+			'columns': ['min_tons','max_tons'],
+			'title_cols': ['Total tonnage (min)','Total tonnage (max)']
+		},
+		{
+			'file_tag': 'economic',
+			'no_access':[-1,1],
+			'legend_label': "(million USD/day)",
+			'divisor': 1000000,
+			'columns': ['min_econ_l','max_econ_l'],
+			'title_cols': ['Economic losses (min)','Economic losses (max)']
+		},
+		{
+			'file_tag': 'total',
+			'no_access':[0,1],
+			'legend_label': "(million USD/day)",
+			'divisor': 1000000,
+			'columns': ['min_loss','max_loss'],
+			'title_cols': ['Total economic impact (min)','Total economic impact (max)']
+		}
+
+	]
+
+	color = '#006d2c'
+	color_by_type = {'Rail Line': color}
+	
+	for plot_set in plot_sets:
+		for c in range(len(plot_set['columns'])):
+			ax = get_axes()
+			plot_basemap(ax, config['paths']['data'])
+			scale_bar(ax, location=(0.8, 0.05))
+			plot_basemap_labels(ax, config['paths']['data'])
+			proj_lat_lon = ccrs.PlateCarree()
+			
+			column = plot_set['columns'][c]
+			weights = [
+				record.attributes[column]
+				for record in shpreader.Reader(flows_file).records()
+				if int(record.attributes['no_access']) in plot_set['no_access']
+			]
+			
+			max_weight = max(weights)
+			width_by_range = generate_weight_bins(weights)
+		
+			geoms_by_range = {}
+			for value_range in width_by_range:
+				geoms_by_range[value_range] = []
+
+			for record in [rec for rec in shpreader.Reader(flows_file).records() if int(rec.attributes['no_access']) in plot_set['no_access']]: 
+				val = record.attributes[column]
+				geom = record.geometry
+				for nmin, nmax in geoms_by_range:
+					if nmin <= val and val < nmax:
+						geoms_by_range[(nmin, nmax)].append(geom)
+
+						# plot
+			for range_, width in width_by_range.items():
+				ax.add_geometries(
+					[geom.buffer(width) for geom in geoms_by_range[range_]],
+					crs=proj_lat_lon,
+					edgecolor='none',
+					facecolor=color,
+					zorder=2)
+	
+			x_l = 102.3
+			x_r = x_l + 0.4
+			base_y = 14
+			y_step = 0.4
+			y_text_nudge = 0.1
+			x_text_nudge = 0.1
+
+			ax.text(
+				x_l,
+				base_y + y_step - y_text_nudge,
+				plot_set['legend_label'],
+				horizontalalignment='left',
+				transform=proj_lat_lon,
+				size=10)
+
+			divisor = plot_set['divisor']
+			for (i, ((nmin, nmax), width)) in enumerate(width_by_range.items()):
+				y = base_y - (i*y_step)
+				line = LineString([(x_l, y), (x_r, y)])
+				ax.add_geometries(
+					[line.buffer(width)],
+					crs=proj_lat_lon,
+					linewidth=0,
+					edgecolor=color,
+					facecolor=color,
+					zorder=2)
+				if nmin == max_weight:
+					label = '>{:.2f}'.format(max_weight/divisor)
+				else:
+					label = '{:.2f}-{:.2f}'.format(nmin/divisor, nmax/divisor)
+				ax.text(
+					x_r + x_text_nudge,
+					y - y_text_nudge,
+					label,
+					horizontalalignment='left',
+					transform=proj_lat_lon,
+					size=10)
+			
+			plt.title(plot_set['title_cols'][c], fontsize = 14)
+			output_file = os.path.join(config['paths']['figures'], 'rail_failure-map-{}-{}.png'.format(plot_set['file_tag'],column))
+			save_fig(output_file)
+			plt.close()
+
+if __name__ == '__main__':
+	main()
