e98833fdd4cf7fa80ad753d3253576785f293ace
Tom Russell <tomalrussell@gmail.com>
2018-11-04 18:58:09 +0700

    Fix whitespace

--- src/vtra/failure/failure_estimation_provinces.py
+++ src/vtra/failure/failure_estimation_provinces.py
@@ -3,8 +3,10 @@ To estimate changing accessibility to commune points
 
 Input data requirements
 -----------------------
+
 1. Correct paths to all files and correct input parameters
-2. Excel sheets with results of flow mapping based on MIN-MAX generalised costs estimates:
+2. Excel sheets with results of flow mapping based on MIN-MAX generalised costs estimates
+
     - origin - String node ID of Origin
     - destination - String node ID of Destination
     - min_edge_path - List of string of edge ID's for paths with minimum generalised cost flows
@@ -18,51 +20,56 @@ Input data requirements
     - min_vehicle_nums - Float values of estimated vehicle numbers for paths with minimum generalised cost flows
     - max_vehicle_nums - Float values of estimated vehicle numbers for paths with maximum generalised cost flows
     - industry_columns - All daily tonnages of industry columns given in the OD matrix data
+
 3. Shapefiles
+
     - edge_id - String/Integer/Float Edge ID
     - geometry - Shapely LineString geomtry of edges
-        
+
 Results
 -------
-Csv sheets with results of failure analysis:
+Csv sheets with results of failure analysis
 
 1. All failure scenarios
+
     - edge_id - String name or list of failed edges
     - origin - String node ID of Origin of disrupted OD flow
     - destination - String node ID of Destination of disrupted OD flow
     - o_region - String name of Province of Origin node ID of disrupted OD flow
     - d_region - String name of Province of Destination node ID of disrupted OD flow
-    - no_access - Boolean 1 (no reroutng) or 0 (rerouting)     
-    - min/max_distance - Float value of estimated distance of OD journey before disruption   
-    - min/max_time - Float value of estimated time of OD journey before disruption     
-    - min/max_gcost - Float value of estimated travel cost of OD journey before disruption    
-    - min/max_vehicle_nums - Float value of estimated vehicles of OD journey before disruption       
-    - new_cost - Float value of estimated cost of OD journey after disruption 
-    - new_distance - Float value of estimated distance of OD journey after disruption    
-    - new_path - List of string edge ID's of estimated new route of OD journey after disruption   
-    - new_time - Float value of estimated time of OD journey after disruption      
-    - dist_diff - Float value of Post disruption minus per-disruption distance  
-    - time_diff - Float value Post disruption minus per-disruption timee  
+    - no_access - Boolean 1 (no reroutng) or 0 (rerouting)
+    - min/max_distance - Float value of estimated distance of OD journey before disruption
+    - min/max_time - Float value of estimated time of OD journey before disruption
+    - min/max_gcost - Float value of estimated travel cost of OD journey before disruption
+    - min/max_vehicle_nums - Float value of estimated vehicles of OD journey before disruption
+    - new_cost - Float value of estimated cost of OD journey after disruption
+    - new_distance - Float value of estimated distance of OD journey after disruption
+    - new_path - List of string edge ID's of estimated new route of OD journey after disruption
+    - new_time - Float value of estimated time of OD journey after disruption
+    - dist_diff - Float value of Post disruption minus per-disruption distance
+    - time_diff - Float value Post disruption minus per-disruption timee
     - min/max_tr_loss - Float value of estimated change in rerouting cost
     - min/max_netrev - Float values of total daily net revenues along disrupted OD pairs
     - min/max_tons - Float values of total daily crop tonnages along disrupted OD pairs
     - min_max_econ_impact - Float values of total daily economic impact of disrupted OD pairs
 
 2. Min-max combined scenarios - Combined min-max results of total network impacts of each edge
+
     - edge_id - String name or list of failed edges
     - no_access - Boolean 1 (no reroutng) or 0 (rerouting)
     - min/max_tr_loss - Float values of estimated change in rerouting cost
     - min/max_tons - Float values of total daily tonnages along edge
     - min/max_netrev - Float values of total daily net revenues along edge
-    - min/max_econ_impact - Float value of total daily economic impact of edge 
+    - min/max_econ_impact - Float value of total daily economic impact of edge
 
 3. Shapefiles - Combined min-max reults of total network impacts of each edge
+
     - edge_id - String name or list of failed edges
     - no_access - Boolean 1 (no reroutng) or 0 (rerouting)
     - min/max_tr_loss - Float values of estimated change in rerouting cost
     - min/max_tons - Float values of total daily tonnages along edge
     - min/max_netrev - Float values of total daily net revenues along edge
-    - min/max_econ_impact - Float value of total daily economic impact of edge     
+    - min/max_econ_impact - Float value of total daily economic impact of edge
     - geometry - Shapely LineString geomtry of edges
 
 """
@@ -84,30 +91,31 @@ from vtra.transport_flow_and_failure_functions import *
 
 
 def main():
-    """
-    Specify the paths from where you want to read and write:
+    """Specify the paths from where you want to read and write:
+
     1. Input data
     2. Intermediate calcuations data
     3. Output results
 
     Supply input data and parameters
+
     1. Names of Provinces
         List of strings
     2. Unit weight of vehicle assumed for each mode
-        List of float types 
-    3. Min-max names of names of different types of attributes - paths, distance, time, cost, vehicles, tons, revenue   
+        List of float types
+    3. Min-max names of names of different types of attributes - paths, distance, time, cost, vehicles, tons, revenue
         List of string types
     4. Percentage of OD flows that are assumed disrupted
         List of float type
     5. Condition on whether analysis is single failure or multiple failure
-        Boolean condition True or False 
+        Boolean condition True or False
 
     Give the paths to the input data files:
     1. Network edges Excel and shapefiles
     2. OD flows Excel file
     3. Failure scenarios Excel file
-    
-    Specify the output files and paths to be created 
+
+    Specify the output files and paths to be created
     """
     data_path, calc_path, output_path = load_config()['paths']['data'], load_config()[
         'paths']['calc'], load_config()['paths']['output']
@@ -137,7 +145,7 @@ def main():
     fail_scenarios_data = os.path.join(
         output_path, 'hazard_scenarios', 'province_roads_hazard_intersections.xlsx')
 
-    """Specify the output files and paths to be created 
+    """Specify the output files and paths to be created
     """
     shp_output_path = os.path.join(output_path, 'failure_shapefiles')
     if os.path.exists(shp_output_path) == False:
@@ -190,7 +198,7 @@ def main():
                 print ('* Loading {} flow paths'.format(province))
                 flow_excel_file = 'province_roads_commune_center_access_flow_paths_{}_percent.xlsx'.format(int(perct))
                 flow_df = pd.read_excel(
-                    os.path.join(flow_paths_data,flow_excel_file), 
+                    os.path.join(flow_paths_data,flow_excel_file),
                     sheet_name = province_name + '_{}_tons'.format(int(tr_wt)))
 
                 """Perform failure analysis
@@ -222,14 +230,14 @@ def main():
                         dist_types[t],time_types[t],cost_types[t],vehicle_types[t],changing_tonnages=False)
 
                     del df
-                    
+
                     tr_loss = '{}_tr_loss'.format(types[t])
                     econ_impact = '{}_econ_impact'.format(types[t])
 
                     flow_df_select.rename(columns={'tr_loss': tr_loss}, inplace=True)
                     flow_df_select[econ_impact] = flow_df_select['no_access']*flow_df_select[rev_types[t]] + \
                         flow_df_select[tr_loss]
-                    
+
                     if single_edge == True:
                         file_name = 'single_edge_failures_all_{0}_{1}_tons_{2}_percent_disrupt.csv'.format(province_name, int(tr_wt),int(perct))
                     else:
@@ -237,7 +245,7 @@ def main():
 
                     df_path = os.path.join(all_fail_scenarios,file_name)
                     flow_df_select.to_csv(df_path, index=False)
-                    
+
                     print ('* Combining {} {} rerouting and isolation failure results'.format(types[t],province))
                     edge_impact = flow_df_select[['edge_id','no_access',tr_loss,tons_types[t],rev_types[t],econ_impact]]
                     edge_impact = edge_impact.groupby(
@@ -248,7 +256,7 @@ def main():
                 edge_impact = edge_fail_ranges[0]
                 edge_impact = pd.merge(edge_impact, edge_fail_ranges[1], how='left', on=[
                                        'edge_id','no_access']).fillna(0)
-                
+
                 del edge_fail_ranges
                 if single_edge == True:
                     file_name = 'single_edge_failures_minmax_{0}_{1}_tons_{2}_percent_disrupt'.format(province_name, int(tr_wt),int(perct))
